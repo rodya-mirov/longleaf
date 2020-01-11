0e22409cffa62a2bb8db3c95846e87becd25d892
Richard Rast <richard.rast@gmail.com>
2020-01-11 13:15:01 -0500

    refactor: remove unsafe, remove parallel feature (all parallel all the time)

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,12 @@ use parser::ReplInput;
 use vm::VM;
 
 fn main() {
-    #[cfg(feature = "parallel")]
-    {
-        rayon::ThreadPoolBuilder::new()
-            .num_threads(8)
-            .build_global()
-            .unwrap();
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(4)
+        .build_global()
+        .unwrap();
 
-        println!("Initialized with {} threads", rayon::current_num_threads());
-    }
-    #[cfg(not(feature = "parallel"))]
-    {
-        println!("Initialized in single-threaded mode");
-    }
+    println!("Initialized with {} threads", rayon::current_num_threads());
 
     println!("Welcome to the longleaf REPL. For help, type 'help'.");
     print!("> ");
